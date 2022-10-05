6239a33be1f1096e03047ad4d10705cfeda4ecd1
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2022-10-05 17:08:17 +0600

    fs write method

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ const fs = require('fs');
 
 const server = http.createServer( function (req, res) {
     if(req.url == "/"){
-        fs.readFileSync('index.html', function (err, data) {
+        fs.writeFile("data.txt", "hello data" , function(err){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write("File created successfully");
+        });  
+    }
+    else if(req.url == "/read"){
+        let data = fs.readFileSync("index.html");{
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);
             res.end();
-        });
+        };
     }
 });
-server.listen(8050);
-console.log("Server is running on port 5050");
+server.listen(3535);
+console.log("Server is running on port 3535");
