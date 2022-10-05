caf39894912555b20bd9fcb03418a4467e64b346
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2022-10-05 17:41:09 +0600

    fs modules create a directory

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer( function (req, res) {
     if(req.url == "/"){
-       fs.rename("data1.txt", "data-file.txt", function(error){
+       fs.unlink("data-file.txt", function(error){
             if(error){
                 res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write("File name fail");
+                res.write("File unlink fail");
                 res.end();
             }
             else{
                 res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write("File name success");
+                res.write("File unlink success");
+                res.end();
+            }
+        });
+    }
+    else if(req.url == "/folder"){
+        fs.mkdir(path.join(__dirname, 'test2'), function(error){
+            if(error){
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write("Directory creation fail");
+                res.end();
+            }
+            else{
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write("Directory creation success");
                 res.end();
             }
         });
